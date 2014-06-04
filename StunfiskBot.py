8797ef6a0d9528d3b69218331119842848e90bbb
Matthew Gee <geem2014@gmail.com>
2014-06-04 13:08:27 -0700

    Initial Commit

---
+++ StunfiskBot.py
@@ -0,0 +1,23 @@
+import praw, argparse, sys, json
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-u', '--username', help='username')
+parser.add_argument('-p', '--password', help='password')
+
+args = parser.parse_args()
+
+user_agent = "StunfiskHelperBot v0.1 by /u/0ffkilter"
+
+reddit = praw.Reddit(user_agent = user_agent)
+
+def main():
+    comments = praw.helpers.comment_stream(reddit, 'KilterBots', limit=None, verbosity=0)
+    for comment in comments:
+        print(comment.body)
+    print("Done")
+
+
+main()
+
+
