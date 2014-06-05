f2afff5e7065a6cb2153a9c76a7f36eb6d128fa3
Matthew Gee <geem2014@gmail.com>
2014-06-04 21:29:44 -0700

    preliminary commit for learnset matches

--- StunfiskBot.py
+++ StunfiskBot.py
@@ -1,4 +1,4 @@
-import praw, argparse, sys, json, pprint
+import praw, argparse, sys, json, re
 
 parser = argparse.ArgumentParser()
 
@@ -11,18 +11,49 @@ user_agent = "StunfiskHelperBot v0.1 by /u/0ffkilter"
 
 reddit = praw.Reddit(user_agent = user_agent)
 
+learn_types = { 'M': 'A TM', 'L': 'Level Up', 'T': 'A Move Tutor', 'S': 'An Event', 'E': "an Egg Move"}
+
+learn_regex = re.compile('(?<=can )\w+|(?= learn)\w+')
+
 def main():
-
-    file = open('Learnsets.json', 'r')
-    learnsets = json.load(file)
-    file.close()
-
-    learnsets['bulbasaur']['learnset']
 
     comments = praw.helpers.comment_stream(reddit, 'KilterBots', limit=None, verbosity=0)
     for comment in comments:
-        print(comment.body)
-    print("Done")
+        if '+stunfiskhelp' in comment.body:
+            print(keys_to_string(can_learn(comment.body.split()[1], comment.body.split()[2])))
+
+
+def can_learn(pokemon, move):
+    if move in learnsets[pokemon]['learnset']:
+        return learnsets[pokemon]['learnset'][move]
+    else:
+        if 'prevo' in pokedex[pokemon]:
+            return can_learn(pokedex[pokemon]['prevo'], move)
+        else:
+            return null
+
+def keys_to_string(keys):
+    if keys:
+        result = ''
+        for key in keys:
+            result = result + gen_string(key) + '\n'
+        return result
+    else:
+        return 'No Results Found'
+
+def gen_string(key):
+    string = 'Generation ' + key[0] + ' through ' + learn_types[key[1]]
+    if key[1] == 'l':
+        string = string + ' at Level ' + key[2::w]
+    return string
+
+file = open('Learnsets.json', 'r')
+learnsets = json.load(file)
+file.close()
+
+file = open('Pokedex.json', 'r')
+pokedex = json.load(file)
+file.close()
 
 
 main()
